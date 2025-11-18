2cfe0e31dee406ca89ac02be8c7cb3f9239ffd35
root <bender@2741c2d51405.(none)>
2025-11-18 23:06:02 +0000

    moar adders

--- src/project.v
+++ src/project.v
@@ -21,8 +21,8 @@ module tt_um_mostparsingvex1(
   wire[7:0] data_in;
   //assign data_in[3:0] = ui_in[3:0];
   //assign data_in[7:4] = 4'h0;
-  assign data_in[4:0] = ui_in[4:0];
-  assign data_in[7:5] = 3'h0;
+  assign data_in[5:0] = ui_in[5:0];
+  assign data_in[7:6] = 2'h0;
   amx_core1 U0( .clk( clk ), .rst( rst ), .data_in( data_in ), .data_out( uo_out ) );
 
   // All output pins must be assigned. If not used, assign to 0.
